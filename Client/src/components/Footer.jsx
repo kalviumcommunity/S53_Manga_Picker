b5bf0d395d81adaed22a08400f8bca76b2040eab
KRISHNA PRASATH <kprasath@kalvium.community>
2024-02-12 11:52:33 +0530

    landing page

---
+++ Client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <div className="flex absolute bottom-0 w-full">
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content mt-auto">
+            <aside>
+                <p>Copyright © 2024 - All right reserved by KP Industries Ltd</p>
+            </aside>
+        </footer>
+      
+    </div>
+  )
+}
+
+export default Footer
