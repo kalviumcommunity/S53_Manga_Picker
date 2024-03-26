7206ae541e96fb77b768a4c8ab5e4b11d9117d57
KRISHNA PRASATH <142793664+EmperorKP@users.noreply.github.com>
2024-03-26 12:46:20 +0530

    Update Data.jsx

--- Client/src/components/Data.jsx
+++ Client/src/components/Data.jsx
@@ -15,7 +15,7 @@ function Data() {
   , []);
 
   const fetchData = async() => {
-    axios.get("https://manga-picker.onrender.com/api")
+    axios.get("https://manga-kappa.vercel.app/api")
       .then((res) => {
         console.log(res)
         setData(res.data.data);
@@ -27,7 +27,7 @@ function Data() {
 
   const handleDelete = (_id) => {
     axios
-      .delete(`https://manga-picker.onrender.com/api/delete/${_id}`)
+      .delete(`https://manga-kappa.vercel.app/api/delete/${_id}`)
       .then(res => console.log(res))
       .catch(err => console.error(err));
     console.log("successful", _id)
