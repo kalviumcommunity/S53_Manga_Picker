287a4a722ac4e6464b812078187bd54923ffa435
KRISHNA PRASATH <142793664+EmperorKP@users.noreply.github.com>
2024-03-26 12:43:47 +0530

    Update FetchData.jsx

--- Client/src/components/FetchData.jsx
+++ Client/src/components/FetchData.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const FetchData = async () => {
   try {
-    const res = await axios.get("https://manga-picker.onrender.com/api");
+    const res = await axios.get("https://manga-kappa.vercel.app/api");
     console.log(res);
     return res.data.data;
   } catch (err) {
